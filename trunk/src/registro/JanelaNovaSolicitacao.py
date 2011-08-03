e271d9f6aaaa18430e883b6ce803c5dd6a22c0c8
alezyoliveiraxD@gmail.com <alezyoliveiraxD@gmail.com@c6ec3542-9036-6a7e-1a7f-9d72e0d9c3cb>
2011-08-03 00:55:50 +0000

    Adoção da Licença GNU GPL v3.0

--- trunk/src/registro/JanelaNovaSolicitacao.py
+++ trunk/src/registro/JanelaNovaSolicitacao.py
@@ -1,4 +1,29 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
+
+'''
+    @author: alezy oliveira lima
+    @contact: alezyoliveira[@]msn[.]com
+    @license: GNU GPL v3.0
+
+    
+    Database tool for request registration management
+    ( Núcleo Setorial de Registro e Controle Acadêmico - UNEAL Campus III
+        Palmeira dos Índios - Alagoas - Brazil )
+    Copyright (C) 2011  Alezy Oliveira Lima
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 from qt import *
 
